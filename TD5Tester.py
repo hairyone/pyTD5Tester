0c3f17de694712aef31c03e7511a0da0ed1cb64f
Hairyone <aaron.trewern@gmail.com>
2017-10-25 23:17:55 +0100

    More re-factoring.
    Put requests into a namedTuple.
    Added support for CAUTIOUS_READ, not yet tested.
    Consider removing serialext and replace with uart.read_data(expected_len
    , timeout).

--- TD5Tester.py
+++ TD5Tester.py
@@ -1,25 +1,33 @@
 from pyftdi.ftdi import Ftdi
+import pyftdi.serialext
 import time
+from collections import namedtuple
 
-READ_BUFFER_SIZE    = 127
+CAUTIOUS_READ           = True
+READ_BUFFER_SIZE        = 127
+SEND_REQUEST_DELAY      = 55
+READ_RESPONSE_TIMEOUT   = 100
 
-INIT_FRAME          = bytearray([0x81, 0x13, 0xF7, 0x81, 0x0C])
-START_DIAGNOSTICS   = bytearray([0x02, 0x10, 0xA0, 0xB2])
-REQUEST_SEED        = bytearray([0x02, 0x27, 0x01, 0x2A])
-KEY_RETURN          = bytearray([0x04, 0x27, 0x02, 0x00, 0x00, 0x00])
-BATTERY_VOLTAGE     = bytearray([0x02, 0x21, 0x10, 0x00]);
+Request = namedtuple('Request', ['request', 'expected_len'])
 
-response = None
-connected = False
+X_INIT_FRAME            = Request(bytearray([0x81, 0x13, 0xF7, 0x81, 0x0C]),        7)
+X_START_DIAGNOSTICS     = Request(bytearray([0x02, 0x10, 0xA0, 0xB2]),              3)
+X_REQUEST_SEED          = Request(bytearray([0x02, 0x27, 0x01, 0x2A]),              6)
+X_KEY_RETURN            = Request(bytearray([0x04, 0x27, 0x02, 0x00, 0x00, 0x00]),  4)
+X_BATTERY_VOLTAGE       = Request(bytearray([0x02, 0x21, 0x10, 0x00]),              8)
+X_ENGINE_RPM            = Request(bytearray([0x02, 0x21, 0x09, 0x00]),              6)
+
+response = None 
+connected = False   
 uart = None
+port = None
 
 ################################################################################
-def pause(ms):
+def pause(delay, step_size):
 ################################################################################
-    s = ms / 1000
-    end_time = time.monotonic() + s
+    end_time = time.monotonic() + delay
     while (time.monotonic() <= end_time):
-        time.sleep(0.01)
+        time.sleep(step_size)
 
 ################################################################################
 def calculate_checksum(request):
@@ -42,25 +50,33 @@ def log_data(data, is_tx):
     ))
 
 ################################################################################
-def get_pid(request):
+def get_pid(request, expected_len):
 ################################################################################
     global response
     
     result = False
 
-    # Punch the calculated calculate_checksum into the last byte and then send the request
+    # Punch the calculated checksum into the last byte and then send the request
     request_len = len(request)
     request[request_len - 1] = calculate_checksum(request)
-    log_data(request, True)
+    if not connected:
+        log_data(request, True)
 
     # Send the request but wait a little to give the ECU a chance to respond to
-    # the previous request
-    pause(50)
-    uart.write_data(request)
+    # any previous request
+    # pause(SEND_REQUEST_DELAY / 1000, 0.0025)
+    port.write(request)
 
+    pause(0.055, 0.01)
     # read the response
-    response = uart.read_data(READ_BUFFER_SIZE)    
-    log_data(response, False)
+    response = None
+    if CAUTIOUS_READ:
+        response = port.read(READ_BUFFER_SIZE)
+    else:
+        response = port.read(expected_len)
+        
+    if not connected:
+        log_data(response, False)
 
     # The request is echoed in the response so we slice it out just leaving the
     # actual response.
@@ -74,7 +90,7 @@ def get_pid(request):
         if cs1 == cs2:
             # Negative response ?
             if response[1] != 0x7F:
-                result = true
+                result = True
 
     return result
 
@@ -98,13 +114,22 @@ def calculate_key(seed):
 def fast_init():
 ################################################################################
     global uart
+    global port
+    global X_KEY_RETURN
+    global response
+    global connected
     
     HI = bytearray([0x01])
     LO = bytearray([0x00])
 
     # set up the device
     uart = Ftdi()
-    uart.open(0x403, 0x6001)
+    try:
+        uart.open(0x403, 0x6001)
+    except Exception as e:
+        print("error={}".format(e))
+        return
+    
     uart.set_baudrate(10400)
     uart.set_line_property(8, 1, 'N')
 
@@ -112,13 +137,13 @@ def fast_init():
     uart.set_bitmode(0x01, 0x01)
     
     uart.write_data(HI)
-    pause(500)
+    pause(0.500, 0.01)
 
     uart.write_data(LO)
-    pause(25)
+    pause(0.0245, 0.00025)
 
     uart.write_data(HI)
-    pause(100)
+    pause(0.100, 0.00025)
 
     # Switch off bit bang
     uart.set_bitmode(0x00, 0x00)
@@ -135,12 +160,18 @@ def fast_init():
     # >> 04 27 02 14 89 CA
     # << 04 27 02 14 89 CA 02 67 02 6B
 
-    if get_pid(INIT_FRAME) and get_pid(START_DIAGNOSTICS) and get_pid(REQUEST_SEED):
+    port = pyftdi.serialext.serial_for_url('ftdi://0x403:0x6001/1', baudrate=10400, bytesize=8, stopbits=1, parity='N')
+    port.timeout = READ_RESPONSE_TIMEOUT / 1000
+
+    if (    get_pid(X_INIT_FRAME.request, X_INIT_FRAME.expected_len)
+        and get_pid(X_START_DIAGNOSTICS.request, X_START_DIAGNOSTICS.expected_len)
+        and get_pid(X_REQUEST_SEED.request, X_REQUEST_SEED.expected_len)
+        ):
         seed = response[3] << 8 | response[4]
         key_hi, key_lo = calculate_key(seed)
-        KEY_RETURN[3] = key_hi
-        KEY_RETURN[4] = key_lo
-        connected = get_pid(KEY_RETURN)
+        X_KEY_RETURN.request[3] = key_hi
+        X_KEY_RETURN.request[4] = key_lo
+        connected = get_pid(X_KEY_RETURN.request, X_KEY_RETURN.expected_len)
 
 ################################################################################
 def start_logger():
@@ -150,8 +181,17 @@ def start_logger():
 
     while True:
         
-        if get_pid(BATTERY_VOLTAGE):
-            print("Battery voltage: {:06.2f}".format(response[5] << 8 | response[6] / 1000.0))
+        if get_pid(X_BATTERY_VOLTAGE.request, X_BATTERY_VOLTAGE.expected_len):
+            print("=={:>20}: {:06.2f}".format(
+                "Battery voltage",
+                (response[5] << 8 | response[6]) / 1000.0
+            ))
+
+        if get_pid(X_ENGINE_RPM.request, X_ENGINE_RPM.expected_len):
+            print("=={:>20}: {:06d}".format(
+                "Engine RPM",
+                (response[3] << 8 | response[4])
+            ))
         
 ################################################################################
 if __name__ == "__main__":
